80f5c72939db42c727e02c31eb5de0ed0329d771
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2026-03-03 20:16:29 -0300

    refactor: remove unnecessary whitespace and improve code readability in core.py

--- nmc/core.py
+++ nmc/core.py
@@ -4,17 +4,14 @@ import subprocess
 from pathlib import Path
 from filesystem import directory as dir
 
-
 def safe_rmtree(path: Path):
     subprocess.run(["rm", "-rf", str(path)], check=False)
-
 
 def format_size(size):
     for unit in ["B", "KB", "MB", "GB", "TB"]:
         if size < 1024:
             return f"{size:.2f} {unit}"
         size /= 1024
-
 
 def find_node_modules(base_path: Path, days: int, min_size_mb: int):
     now = time.time()
@@ -39,7 +36,6 @@ def find_node_modules(base_path: Path, days: int, min_size_mb: int):
                 continue
 
     return found
-
 
 def cleanup(found, dry_run: bool):
     for path, _ in found:
